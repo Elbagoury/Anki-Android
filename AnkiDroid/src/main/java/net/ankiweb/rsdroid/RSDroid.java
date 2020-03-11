937c5399e85cd75f6f9d1d09b45d9537286769d0
Damien Elmes <gpg@ankiweb.net>
2020-03-11 12:26:53 +1000

    add timingToday()

--- AnkiDroid/src/main/java/net/ankiweb/rsdroid/RSDroid.java
+++ AnkiDroid/src/main/java/net/ankiweb/rsdroid/RSDroid.java
@@ -1,13 +1,32 @@
 package net.ankiweb.rsdroid;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class RSDroid {
     static {
         System.loadLibrary("rsdroid");
     }
 
-    private static native String request(final String input);
+    private static native String request(final String command, final String args);
 
-    public static String runCommand(String to) {
-        return request(to);
+    // returns (daysElapsed, nextDayAt)
+    public static int[] timingToday(int createdSecs, int createdMinsWest, int rolloverHour) {
+        try {
+            JSONObject i = new JSONObject();
+            i.put("created_secs", createdSecs);
+            i.put("created_mins_west", createdMinsWest);
+            i.put("rollover_hour", rolloverHour);
+
+            String output = request("timingToday", i.toString());
+
+            JSONObject obj = new JSONObject(output);
+            int[] o = new int[2];
+            o[0] = obj.getInt("days_elapsed");
+            o[1] = obj.getInt("next_day_at");
+            return o;
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- rsdroid/src/lib.rs
+++ rsdroid/src/lib.rs
@@ -2,23 +2,64 @@ use jni::objects::JClass;
 use jni::objects::JString;
 use jni::sys::jstring;
 use jni::JNIEnv;
+use serde_derive::{Deserialize, Serialize};
+use serde_json;
 
-fn transform_string(s: String) -> String {
-    format!("hello {}", s)
+use anki::sched::{local_minutes_west_for_stamp, sched_timing_today};
+use std::time;
+
+#[derive(Deserialize)]
+struct TimingTodayIn {
+    created_secs: i64,
+    created_mins_west: i32,
+    rollover_hour: i8,
+}
+
+#[derive(Serialize)]
+struct TimingTodayOut {
+    days_elapsed: u32,
+    next_day_at: i64,
+}
+
+fn run_command(command: String, args: String) -> String {
+    match command.as_str() {
+        "timingToday" => {
+            let i: TimingTodayIn = serde_json::from_str(&args).unwrap();
+            let now_stamp = time::SystemTime::now()
+                .duration_since(time::SystemTime::UNIX_EPOCH)
+                .unwrap()
+                .as_secs();
+            let now_mins_west = local_minutes_west_for_stamp(now_stamp as i64) as i32;
+            let o = sched_timing_today(
+                i.created_secs,
+                i.created_mins_west,
+                now_stamp as i64,
+                now_mins_west,
+                i.rollover_hour,
+            );
+            let out = TimingTodayOut {
+                days_elapsed: o.days_elapsed,
+                next_day_at: o.next_day_at,
+            };
+            serde_json::to_string(&out).unwrap()
+        }
+        _ => {
+            panic!("unexpected command: {}", command);
+        }
+    }
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn Java_net_ankiweb_rsdroid_RSDroid_request(
     env: JNIEnv,
     _: JClass,
-    java_pattern: JString,
+    command: JString,
+    args: JString,
 ) -> jstring {
-    let input_str: String = env
-        .get_string(java_pattern)
-        .expect("invalid pattern")
-        .into();
+    let command_str: String = env.get_string(command).expect("invalid command").into();
+    let args_str: String = env.get_string(args).expect("invalid args").into();
 
-    let output_str = transform_string(input_str);
+    let output_str = run_command(command_str, args_str);
 
     let output = env
         .new_string(output_str)
